03c3afd54f94e5ed02c2b06eb025a8fa42427baa
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2015-12-30 18:24:02 +0100

    initial commit for DryrBluetooth

---
+++ Software/Base/Bluetooth/src/dryr/base/bluetooth/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package dryr.base.bluetooth;
+
+/**
+ * @author addis
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
