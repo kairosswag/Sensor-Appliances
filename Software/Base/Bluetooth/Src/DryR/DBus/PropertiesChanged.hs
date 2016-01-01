5736e88fccec1fbe0f7a19868450f5b83947b651
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-01 23:03:37 +0100

    Base/Bluetooth: Implement PropertiesChanged Parser

---
+++ Software/Base/Bluetooth/Src/DryR/DBus/PropertiesChanged.hs
@@ -0,0 +1,22 @@
+module DryR.DBus.PropertiesChanged where
+
+import Data.Maybe
+
+import DBus
+
+data PropertiesChanged = PropertiesChanged  {
+  objectPath :: ObjectPath,
+  interfaceName :: InterfaceName,
+  changedProperties :: Dictionary,
+  invalidatedProperties :: [String]}
+
+parseSignalToPropertiesChanged :: Signal -> Maybe PropertiesChanged
+parseSignalToPropertiesChanged s =
+  if (length $ sb) == 3 && (isJust iN) && (isJust cP) && (isJust iP)
+  then Just $ PropertiesChanged (signalPath s) (fromJust iN) (fromJust cP) (fromJust iP)
+  else Nothing
+  where
+    sb = signalBody s
+    iN = fromVariant $ sb!!0
+    cP = fromVariant $ sb!!1
+    iP = fromVariant $ sb!!2
