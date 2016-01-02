57e772ccea18f9322615de4f68cb4e472e471eba
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-02 18:05:49 +0100

    Base/Bluetooth: Add Smart Gadget UUIDs

---
+++ Software/Base/Bluetooth/Src/DryR/Sensirion.hs
@@ -0,0 +1,7 @@
+module DryR.Sensirion where
+
+sv_tmp = "00002234-b38d-4985-720e-0f993a68ee41"
+sv_hum = "00001234-b38d-4985-720e-0f993a68ee41"
+
+ch_tmp = "00002235-b38d-4985-720e-0f993a68ee41"
+ch_hum = "00001235-b38d-4985-720e-0f993a68ee41"
