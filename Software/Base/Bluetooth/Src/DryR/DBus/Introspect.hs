a69a123f26a640a2b5147f3e3b0af25b01cb21a6
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-03 18:47:24 +0100

    Base/Bluetooth: Fix introspect function and request BusName

--- Software/Base/Bluetooth/Src/DryR/DBus/Introspect.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/Introspect.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module DryR.DBus.Introspect where
 
 import Data.Maybe
@@ -6,9 +8,13 @@ import DBus
 import DBus.Client
 import DBus.Introspection
 
-introspect :: Client -> ObjectPath -> IO (Object)
-introspect client oP = do
-  let mc = methodCall oP (interfaceName_ "org.freedesktop.DBus.Introspectable") (memberName_ "Introspect")
-  mr <- call_ client mc
-  let xml = fromVariant ((methodReturnBody mr)!!0)
-  return $ fromJust $ parseXML oP $ fromJust xml
+introspect :: Client -> BusName -> ObjectPath -> IO (Maybe Object)
+introspect client bN oP = do
+  let mc = (methodCall oP "org.freedesktop.DBus.Introspectable" "Introspect") {
+    methodCallDestination = Just bN
+  }
+
+  r <- call client mc
+  case (r) of
+    Left _ -> return Nothing
+    Right mr -> return $ Just $ fromJust $ parseXML oP $ fromJust $ fromVariant ((methodReturnBody mr)!!0)
