e8190ca334994ce8ada5f0ce0eec76e0a8a2ad39
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-03 17:54:41 +0100

    Base/Bluetooth: Implement Functions to Convert between MAC Adresses and ObjectPaths

---
+++ Software/Base/Bluetooth/Src/DryR/DBus/Util.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module DryR.DBus.Util where
+
+import Data.Char
+import Data.List
+
+import Data.String.Utils
+
+import DBus
+
+macToObjectPath :: ObjectPath -> String -> ObjectPath
+macToObjectPath parent mac = objectPath_ ((formatObjectPath parent) ++ "/dev_" ++ (replace ":" "_" $ map toUpper mac))
+
+objectPathToMac :: ObjectPath -> Maybe String
+objectPathToMac oP = fmap (replace "_" ":") $ stripPrefix "dev_" $ reverse $ takeWhile (/= '/') $ reverse $ formatObjectPath oP
