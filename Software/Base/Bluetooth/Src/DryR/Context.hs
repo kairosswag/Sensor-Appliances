82d725bbc9aa966f7d9fc94c4f8246e36316f053
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-07 13:42:54 +0100

    Base/Bluetooth: Create Context

---
+++ Software/Base/Bluetooth/Src/DryR/Context.hs
@@ -0,0 +1,42 @@
+module DryR.Context where
+
+import Control.Concurrent
+import Control.Concurrent.MVar
+
+import qualified Database.MySQL.Simple as DB
+import qualified DBus.Client as DS
+
+data Context = Context {
+  contextDatabase :: DB.Connection,
+  contextDBus :: DS.Client}
+
+type InnerContext = MVar (Maybe Context)
+
+newContext :: IO (InnerContext)
+newContext = do
+  connDBus <- DS.connectSystem
+  connDatabase <- DB.connect DB.defaultConnectInfo {DB.connectDatabase="dryr.base", DB.connectUser="dryr.base"}
+  newMVar $ Just $ Context connDatabase connDBus
+
+takeContext :: InnerContext -> IO (Maybe Context)
+takeContext vcontext = do
+  mcontext <- swapMVar vcontext Nothing
+  return mcontext
+
+deleteContext :: Context -> IO ()
+deleteContext (Context connDatabase connDBus) = do
+  DB.close connDatabase
+  DS.disconnect connDBus
+
+withContext :: InnerContext -> (Context -> IO ()) -> IO ()
+withContext vcontext func = do
+  mcontext <- takeMVar vcontext
+
+  case (mcontext) of
+    Just context -> do
+      func context
+      putMVar vcontext mcontext
+    Nothing -> return ()
+
+withContextAsync :: InnerContext -> (Context -> IO ()) -> IO (ThreadId)
+withContextAsync vcontext func = forkIO $ withContext vcontext func
