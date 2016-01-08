6012902d878903574228c41a0cfae11a81d17832
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 00:59:19 +0100

    Base/Bluetooth: Add various MethodCalls into DBus

---
+++ Software/Base/Bluetooth/Src/DryR/DBus/MethodCall/Adapter1.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module DryR.DBus.MethodCall.Adapter1 where
+
+import DBus
+import DBus.Client
+
+import DryR.Context
+import DryR.DBus.Properties
+
+powerOn :: Context -> ObjectPath -> IO (Maybe ())
+powerOn c oP = set (contextDBus c) oP "org.bluez.Adapter1" "Powered" True
+
+powerOff :: Context -> ObjectPath -> IO (Maybe ())
+powerOff c oP = set (contextDBus c) oP "org.bluez.Adapter1" "Powered" False
+
+startDiscovery :: Context -> ObjectPath -> IO (Maybe ())
+startDiscovery c oP = do
+  let mc = (methodCall oP ("org.bluez.Adapter1") ("StartDiscovery")) {
+    methodCallDestination = Just "org.bluez"
+  }
+
+  mr <- call (contextDBus c) mc
+  case (mr) of
+    Left _ -> return Nothing
+    Right _ -> return $ Just ()
+
+stopDiscovery :: Context -> ObjectPath -> IO (Maybe ())
+stopDiscovery c oP = do
+  let mc = (methodCall oP ("org.bluez.Adapter1") ("StopDiscovery")) {
+    methodCallDestination = Just "org.bluez"
+  }
+
+  mr <- call (contextDBus c) mc
+  case (mr) of
+    Left _ -> return Nothing
+    Right _ -> return $ Just ()
+
+removeDevice :: Context -> ObjectPath -> ObjectPath -> IO (Maybe ())
+removeDevice c oP device = do
+  let mc = (methodCall oP ("org.freedesktop.Adapter1") ("RemoveDevice")) {
+    methodCallDestination = Just "org.bluez",
+    methodCallBody = [toVariant device]
+  }
+
+  mr <- call (contextDBus c) mc
+  case (mr) of
+    Left _ -> return Nothing
+    Right _ -> return $ Just ()
