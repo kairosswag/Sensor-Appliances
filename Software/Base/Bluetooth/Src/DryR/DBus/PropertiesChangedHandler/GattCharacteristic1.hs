b74ef1e43e1411581f10a3a4d20c0020c19990e1
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 00:08:03 +0100

    Base/Bluetooth: Discard Temperature Tracking

--- Software/Base/Bluetooth/Src/DryR/DBus/PropertiesChangedHandler/GattCharacteristic1.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/PropertiesChangedHandler/GattCharacteristic1.hs
@@ -13,7 +13,6 @@ import DryR.DBus.PropertiesChanged
 import DryR.Sensirion
 
 import DryR.DBus.PropertiesChangedHandler.GattCharacteristic1.Humidity
-import DryR.DBus.PropertiesChangedHandler.GattCharacteristic1.Temperature
 
 gattCharacteristic1Handler pC c = do
   let oP = pCObjectPath pC
@@ -21,10 +20,8 @@ gattCharacteristic1Handler pC c = do
   u <- get c (busName_ "org.bluez") oP (interfaceName_ "org.bluez.GattCharacteristic1") (memberName_ "UUID")
   let us :: String = fromJust $ fromVariant $ fromJust u
 
-  if us == ch_tmp
-    then temperatureHandler pC c
-    else if us == ch_hum
-      then humidityHandler pC c
-      else return ()
+  if us == ch_hum
+    then humidityHandler pC c
+    else return ()
 
   print "GC1"
