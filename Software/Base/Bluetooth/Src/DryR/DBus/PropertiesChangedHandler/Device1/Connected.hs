2868a30a5a43e0152fbbaebe53100d53bc3381f7
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 14:27:06 +0100

    Base/Bluetooth: Start Notify on Connected

--- Software/Base/Bluetooth/Src/DryR/DBus/PropertiesChangedHandler/Device1/Connected.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/PropertiesChangedHandler/Device1/Connected.hs
@@ -2,6 +2,8 @@
 
 module DryR.DBus.PropertiesChangedHandler.Device1.Connected (connectedHandler) where
 
+import Control.Concurrent
+
 import Data.Maybe
 
 import Database.MySQL.Simple
@@ -9,8 +11,10 @@ import Database.MySQL.Simple
 import DBus
 
 import DryR.Context
+import DryR.DBus.MethodCall.GattCharacteristic1
 import DryR.DBus.PropertiesChanged
 import DryR.DBus.Util
+import DryR.Sensirion
 import DryR.SQL.Query
 
 connectedHandler pC c = do
@@ -21,7 +25,16 @@ connectedHandler pC c = do
     Nothing -> return ()
 
 connectedTrue pC c = do
-  let mac = objectPathToMac $ pCObjectPath pC
+  let oP = pCObjectPath pC
+  let mac = objectPathToMac oP
+
+  threadDelay 2000000
+
+  oP_sv_hum <- getGattService1 c oP sv_hum
+  oP_ch_hum <- getGattCharacteristic1 c (fromJust oP_sv_hum) ch_hum
+
+  startNotify c $ fromJust oP_ch_hum
+
   execute (contextDatabase c) (getQuery UpdateDevice $ contextQueries c) (1 :: Integer, mac)
   return ()
 
--- Software/Base/Bluetooth/Src/DryR/DBus/Util.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/Util.hs
@@ -9,6 +9,10 @@ import Data.String.Utils
 
 import DBus
 
+import DryR.Context
+import DryR.DBus.Introspect
+import DryR.DBus.Properties
+
 macToObjectPath :: ObjectPath -> String -> ObjectPath
 macToObjectPath parent mac = objectPath_ ((formatObjectPath parent) ++ "/dev_" ++ (replace ":" "_" $ map toUpper mac))
 
@@ -18,3 +22,25 @@ objectPathToMac oP = fmap (replace "_" ":") $ stripPrefix "dev_" $ reverse $ tak
 parent :: ObjectPath -> ObjectPath
 parent "/" = "/"
 parent oP = objectPath_ $ reverse $ tail $ dropWhile (/= '/') $ reverse $ formatObjectPath oP
+
+getGattService1 :: Context -> ObjectPath -> String -> IO (Maybe ObjectPath)
+getGattService1 c oP uuid = do
+  mgs <- get c "org.bluez" oP "org.bluez.Device1" "GattServices"
+  case (mgs >>= fromVariant) of
+    Just gs -> do
+      uuids <- mapM (\g -> do
+        uuid <- get c "org.bluez" g "org.bluez.GattService1" "UUID"
+        return (uuid >>= fromVariant)) gs
+      return $ lookup (Just uuid) $ zip uuids gs
+    Nothing -> return Nothing
+
+getGattCharacteristic1 :: Context -> ObjectPath -> String -> IO (Maybe ObjectPath)
+getGattCharacteristic1 c oP uuid = do
+  mgc <- get c "org.bluez" oP "org.bluez.GattService1" "Characteristics"
+  case (mgc >>= fromVariant) of
+    Just gc -> do
+      uuids <- mapM (\g -> do
+        uuid <- get c "org.bluez" g "org.bluez.GattCharacteristic1" "UUID"
+        return (uuid >>= fromVariant)) gc
+      return $ lookup (Just uuid) $ zip uuids gc
+    Nothing -> return Nothing
