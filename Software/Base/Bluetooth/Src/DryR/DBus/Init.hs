8d630a32e05a615507ff42151563d99a31f0f290
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 19:55:57 +0100

    Base/Bluetooth: Disconnect Devices on Shutdown

--- Software/Base/Bluetooth/Src/DryR/DBus/Init.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/Init.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module DryR.DBus.Init where
 
+import Database.MySQL.Simple
+
 import DryR.Context
 import DryR.DBus.MethodCall.Adapter1
+import DryR.DBus.MethodCall.Device1
+import DryR.DBus.Util
+import DryR.SQL.Query
 
 initialize :: InnerContext -> IO (Maybe ())
 initialize = withContext (\c -> do
@@ -13,6 +19,12 @@ initialize = withContext (\c -> do
 
 uninitialize :: InnerContext -> IO (Maybe ())
 uninitialize = withContext (\c -> do
+
+  qr :: [Only String] <- query_ (contextDatabase c) (getQuery SelectDeviceConnected $ contextQueries c)
+  let connected_devices = map ((macToObjectPath "/org/bluez/hci0") . fromOnly) qr
+
+  mapM_ (disconnect c) connected_devices
+
   stopDiscovery c "/org/bluez/hci0"
   powerOff c "/org/bluez/hci0"
   return ())
