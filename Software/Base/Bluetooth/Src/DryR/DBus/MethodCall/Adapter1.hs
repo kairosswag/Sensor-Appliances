48ebb12fdb15da7f1347d33718b149ddd9bfe59a
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 17:32:50 +0100

    Base/Bluetooth: Fix calls to Get/Set and Fix RemoveDevice

--- Software/Base/Bluetooth/Src/DryR/DBus/MethodCall/Adapter1.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/MethodCall/Adapter1.hs
@@ -9,10 +9,10 @@ import DryR.Context
 import DryR.DBus.Properties
 
 powerOn :: Context -> ObjectPath -> IO (Maybe ())
-powerOn c oP = set (contextDBus c) oP "org.bluez.Adapter1" "Powered" True
+powerOn c oP = set c "org.bluez" oP "org.bluez.Adapter1" "Powered" True
 
 powerOff :: Context -> ObjectPath -> IO (Maybe ())
-powerOff c oP = set (contextDBus c) oP "org.bluez.Adapter1" "Powered" False
+powerOff c oP = set c "org.bluez" oP "org.bluez.Adapter1" "Powered" False
 
 startDiscovery :: Context -> ObjectPath -> IO (Maybe ())
 startDiscovery c oP = do
@@ -38,12 +38,12 @@ stopDiscovery c oP = do
 
 removeDevice :: Context -> ObjectPath -> ObjectPath -> IO (Maybe ())
 removeDevice c oP device = do
-  let mc = (methodCall oP ("org.freedesktop.Adapter1") ("RemoveDevice")) {
+  let mc = (methodCall oP ("org.bluez.Adapter1") ("RemoveDevice")) {
     methodCallDestination = Just "org.bluez",
     methodCallBody = [toVariant device]
   }
 
   mr <- call (contextDBus c) mc
   case (mr) of
-    Left _ -> return Nothing
-    Right _ -> return $ Just ()
+    Left e -> return Nothing
+    Right s -> return $ Just ()
