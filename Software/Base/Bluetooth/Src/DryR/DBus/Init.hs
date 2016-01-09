ddee20dc7a51829a49ff5ff5e42ef3869cd4571c
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-09 18:52:37 +0100

    Implement Initialization

--- Software/Base/Bluetooth/Src/DryR/DBus/Init.hs
+++ Software/Base/Bluetooth/Src/DryR/DBus/Init.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module DryR.DBus.Init where
 
 import DryR.Context
+import DryR.DBus.MethodCall.Adapter1
 
-initialize :: InnerContext -> IO ()
-initialize c = do
-  return ()
+initialize :: InnerContext -> IO (Maybe ())
+initialize = withContext (\c -> do
+  powerOn c "/org/bluez/hci0"
+  startDiscovery c "/org/bluez/hci0"
+  return ())
 
-uninitialize :: InnerContext -> IO ()
-uninitialize c = do
-  return ()
+uninitialize :: InnerContext -> IO (Maybe ())
+uninitialize = withContext (\c -> do
+  stopDiscovery c "/org/bluez/hci0"
+  powerOff c "/org/bluez/hci0"
+  return ())
