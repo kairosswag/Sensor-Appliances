6d2b70e4fc240a9ac8d86e6fb603bba11aa84492
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-21 15:02:42 +0100

    DryRBasestation: Add Servlet DataUpdateHandler

---
+++ Software/DryRBaseStation/basestation-tomcat/src/main/java/dryr/basestation/servlets/DataUpdateHandler.java
@@ -0,0 +1,37 @@
+package dryr.basestation.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dryr.basestation.database.DataPointDB;
+import dryr.basestation.type.MinMax;
+
+public class DataUpdateHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public DataUpdateHandler() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataPointDB dataPointDB = new DataPointDB();
+		if (request.getParameterMap().containsKey("device")) {
+			String mac = request.getParameter("device");
+			MinMax minMax = dataPointDB.getMinMax(mac, 60);
+			if (minMax != null && minMax.getMax() - minMax.getMin() > 10.0) {
+				dataPointDB.deleteData(mac);
+			}
+		}
+		dataPointDB.deleteOldData(5);
+	}
+}
