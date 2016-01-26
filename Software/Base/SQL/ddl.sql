95feec0ee654ba33b4cb7c7522182ea3e2fb540a
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-26 18:10:50 +0100

    DDL: Create View for Minute-Based Average Humidity

--- Software/Base/SQL/ddl.sql
+++ Software/Base/SQL/ddl.sql
@@ -22,3 +22,8 @@ CREATE TABLE Humidity (
   ON UPDATE CASCADE
   ON DELETE CASCADE
 );
+
+CREATE VIEW Group_Humidity AS
+SELECT DATE_SUB(sample_time, INTERVAL SECOND(sample_time) SECOND) AS group_time, mac, AVG(value)
+FROM `Humidity`
+GROUP BY mac, group_time;
