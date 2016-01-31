b8a4c86065003e2da13c5f73ed942bd98f8f4022
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-01-31 18:51:32 +0100

    Util: Configure shooould work now hopefully

--- Util/configure-dryr-base-bluetooth-system.sh
+++ Util/configure-dryr-base-bluetooth-system.sh
@@ -1,3 +1,4 @@
 sed -i '/^ExecStart/ s/$/ -E/' /etc/systemd/system/bluetooth.target.wants/bluetooth.service
-cp dryr.base.bluetooth.conf /etc/dbus-1/system.d/
+sed -i '/^ExecStart/ s/$/ -E/' /etc/systemd/system/dbus-org.bluez.service
+systemctl daemon-reload
 groupadd dryr
